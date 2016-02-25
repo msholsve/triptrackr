a6f0f423ecc48d31e5f9a596b2be78272c12da97
Jakob Svennevik Notland <jakobsn@stud.ntnu.no>
2016-02-25 11:21:28 +0100

    dbwrapper

---
+++ dbwrapper/dbwrapper.py
@@ -0,0 +1,26 @@
+
+import requests
+
+
+class DbWrapper:
+    def __init__(self, ip, port, username, password):
+        self.ip = ip
+        self.port = port
+        self.username = username
+        self.password = password
+
+    def send(self, data):
+        url = 'http://' + self.ip + ':' + self.port + "/api/post_data"
+        requests.post(url, data=data, auth=(self.username, self.password))
+
+"""
+test = DbWrapper('10.20.80.193', '8080', 'Admin', '1234')
+test.send({'adsas': 1233})
+
+i=0
+
+while(i<20):
+    i+=1
+    test.send({'adsas': i})
+"""
+
