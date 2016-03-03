6d7846b2d673c28ff5a72cdf7e480b870896a476
Michael <michaehg@stud.ntnu.no>
2016-03-03 10:15:21 +0100

    Core should work with the other wrappers now. Mathias can fix the remaining parts (i.e. uncomment and/or make necessary changes) when car is turned into a thread like the gpspoller.

--- Core.py
+++ Core.py
@@ -1,14 +1,18 @@
-
-# import gpspoller
-# import icar
-# import dbwrapper
+from gpswrapper import gpspoller
+from dbwrapper import dbwrapper
+from car import car
 import time
-import threading
+# import threading
+
+# CONFIG
+pollingDelay = 2
+errorPollingDelay = 60
+# ENDCONFIG
 
 if __name__ == '__main__':
-    # gpsp = GpsPoller()
-    # TODO: Hent data fra icar
-    # dbWrapper = DbWrapper('10.20.80.193', '8080', 'Admin', '1234')
+    gpsd = gpspoller.GpsPoller()
+    obd = car.Car()
+    dbWrapper = dbwrapper.DbWrapper('10.20.80.193', '8080', 'Admin', '1234')
 
     reg = "RJ46564"  # TODO: Hent registrering
 
@@ -18,40 +22,49 @@ if __name__ == '__main__':
         'car_name': reg,
         'data': {}
     }
-    try:
-        # gpsp.start()
-        while True:
-            time.sleep(2)  # Data stabilization delay
 
-            # satelliteFix = 0 if gpsd.fix.mode==1 else 1
-            data = {
-                # 'latitude': str(gpsd.fix.latitude * satelliteFix),
-                # 'longitude': str(gpsd.fix.longitude * satelliteFix),
-                # 'fuel_status': ,
-                # 'fuel_rate': ,
-                # 'engine_load': ,
-                # 'rpm': ,
-                # 'speed': ,
-                # 'throttle_position': ,
-                # 'run_time': ,
-                # 'intake_air_temp': ,
-                # 'outside_air_temp: ,
-                # 'oil_temp':
+    timeSinceErrorDetect = 0
+    try:
+        gpsd.start()
+        # obd.start()
+        obd.EnableFetch(obd.GetSupportedDataTypes())
+        time.sleep(2)  # Data stabilization delay
+        while True:
+            error = ""
+            if timeSinceErrorDetect > errorPollingDelay:
+                error = obd.CheckForCarErrors()
+                timeSinceErrorDetect = 0
+            else:
+                timeSinceErrorDetect += pollingDelay
+
+            dataList = obd.FetchData()
+
+            satelliteFix = gpsd.have_fix()
+            data = {  # TODO: Finne ut om det er riktig måte å få tilgang til enumeratoren
+                'latitude': str(gpsd.get_latitude() * satelliteFix),
+                'longitude': str(gpsd.get_longitude() * satelliteFix),
+                'fuel_status': dataList[obd.DataTypes.FuelStatus],
+                'fuel_rate': dataList[obd.DataTypes.FuelRate],
+                'engine_load': dataList[obd.DataTypes.EngineLoad],
+                'rpm': dataList[obd.DataTypes.RPM],
+                'speed': dataList[obd.DataTypes.Speed],
+                'throttle_position': dataList[obd.DataTypes.ThrottlePosition],
+                'run_time': dataList[obd.DataTypes.RunTime],
+                'intake_air_temp': dataList[obd.DataTypes.IntakeAirTemp],
+                'outside_air_temp:': dataList[obd.DataTypes.OutsideAirTemp],
+                'oil_temp': dataList[obd.DataTypes.OilTemp],
+                'error': error
+
             }
 
             trip['timestamp'] = time.localtime()
             trip['data'] = data
 
-            # dbWrapper.send(trip)
+            dbWrapper.send(trip)
 
-            time.sleep(2)
+            time.sleep(pollingDelay)
 
     except (KeyboardInterrupt, SystemExit):
         print("\nKilling Thread...")
-        # gpsp.running = False
-        # gpsp.join()
-
-
-
-
-
+        gpsd.disconnect()
+        # car.disconnect()
