c6f42d2ae53ed691b2566cc46f5d9b0b3456febd
Max <max@dira.no>
2016-04-05 17:03:36 +0200

    TTR-46: Implemented RFID wrapper

---
+++ rfid/state.py
@@ -0,0 +1,41 @@
+import threading
+import time
+from rfid.keyboard_alike import reader
+
+
+class Identity(threading.Thread):
+    __mutex = threading.Lock()
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.current_value = None
+        self.running = True  # setting the thread running to true
+        self.connected = True
+        self.reader = None
+
+        self.id = None
+
+        self.start()
+
+    def run(self):
+        while self.running:
+
+            if not self.connected:
+                try:
+                    self.reader = reader.Reader(0xffff, 0x0035, 84, 16, should_reset=False)
+                    self.reader.initialize()
+                    self.connected = True
+                except reader.DeviceException as e:
+                    self.connected = False
+            else:
+                self.id = self.reader.read().strip()
+
+        self.reader.disconnect()
+
+    def get_id(self):
+        ret = self.id
+        self.id = None
+        return ret
+
+
